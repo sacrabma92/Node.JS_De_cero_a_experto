afdd602210b3775b0295df009d08169c6fb06155
Carlos Ramirez <karlossander92@gmail.com>
2022-05-28 07:10:15 -0500

    07-restserver

---
+++ 07-restserver/models/server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    // Middlewares
+    this.middlewares();
+
+    // Rutas de mi aplicacion
+    this.routes();
+  }
+
+  middlewares() {
+    //Directorio Público
+    this.app.use(express.static("public"));
+  }
+
+  routes() {
+    this.app.get("/api", (req, res) => {
+      res.json({
+        msg: "get API",
+      });
+    });
+
+    this.app.put("/api", (req, res) => {
+      res.json({
+        msg: "put API",
+      });
+    });
+
+    this.app.post("/api", (req, res) => {
+      res.json({
+        msg: "post API",
+      });
+    });
+
+    this.app.delete("/api", (req, res) => {
+      res.json({
+        msg: "delete API",
+      });
+    });
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Servidor corriendo en puerto", this.port);
+    });
+  }
+}
+
+module.exports = Server;
